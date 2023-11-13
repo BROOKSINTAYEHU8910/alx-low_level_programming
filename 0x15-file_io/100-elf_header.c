35c09955408277779731858ef6d3fb7fb445f916
Brook <sintayehubrook1540@gmail.com>
2023-11-13 12:28:32 -0800

    five

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * check_elf - A function that checks if the file is in ELF format
+ * @e_ident: A Pointer to an array of ELF versions
+ * Return: void
+ */
+void check_elf(unsigned char *e_ident)
+{
+	/* Function body */
+}
+
+/**
+ * print_magic - A function that prints the ELF magic num
+ * @e_ident: A pointer to an array of ELF versions
+ * Return: Nothing
+ */
+void print_magic(unsigned char *e_ident)
+{
+	/* Function body */
+}
+
+/* ... (Other functions) ... */
+
+/**
+ * close_elf - A function that closes the file descriptor for the ELF file
+ * @elf: The file descriptor to be closed
+ * Return: Nothing
+ */
+void close_elf(int elf)
+{
+	/* Function body */
+}
+
+/**
+ * main - The entry point for the ELF header program
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of pointer to the arguments
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	/* Function body */
+}
+
