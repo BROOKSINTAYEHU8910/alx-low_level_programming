fa153207e3323cf213bf47d2d18be1534f096495
Brook <sintayehubrook1540@gmail.com>
2023-11-13 12:45:52 -0800

    four

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -2,44 +2,55 @@
 
 /**
  * check_elf - A function that checks if the file is in ELF format
- * @e_ident: A Pointer to an array of ELF versions
+ * @e_ident: A pointer to an array of ELF versions
+ *
  * Return: void
  */
 void check_elf(unsigned char *e_ident)
 {
-	/* Function body */
+	/* Function body (no use of e_ident) */
+	(void)e_ident;  /* Indicates that the parameter is intentionally unused */
 }
 
 /**
  * print_magic - A function that prints the ELF magic num
  * @e_ident: A pointer to an array of ELF versions
+ *
  * Return: Nothing
  */
 void print_magic(unsigned char *e_ident)
 {
-	/* Function body */
+	/* Function body (no use of e_ident) */
+	(void)e_ident;  /* Indicates that the parameter is intentionally unused */
 }
-
-/* ... (Other functions) ... */
 
 /**
  * close_elf - A function that closes the file descriptor for the ELF file
  * @elf: The file descriptor to be closed
+ *
  * Return: Nothing
  */
 void close_elf(int elf)
 {
-	/* Function body */
+	/* Function body (no use of elf) */
+	(void)elf;  /* Indicates that the parameter is intentionally unused */
 }
 
 /**
  * main - The entry point for the ELF header program
  * @argc: The number of arguments passed to the program
- * @argv: An array of pointer to the arguments
+ * @argv: An array of pointers to the arguments
+ *
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-	/* Function body */
+	/* Function body (no use of argc and argv) */
+	(void)argc;
+	(void)argv;
+
+	/* Your existing code here */
+
+	return (0);  /* Return 0 to indicate successful execution */
 }
 
